ac139ee73601b65b4cdfe55bb81ad6c23302eca1
raimundocoelho-ti <raimundocoelho.ti@gmail.com>
2025-06-11 17:34:10 -0300

    Start map

--- database/seeders/VehicleBrandSeeder.php
+++ database/seeders/VehicleBrandSeeder.php
@@ -16,36 +16,10 @@ class VehicleBrandSeeder extends Seeder
         DB::table('vehicle_brands')->delete(); // Opcional: Limpa a tabela antes de popular
 
         $brands = [
-            // Comuns no Brasil (Carros e Leves)
             ['name' => 'Fiat'],
             ['name' => 'Volkswagen'],
-            ['name' => 'Chevrolet'],
-            ['name' => 'Ford'],
             ['name' => 'Renault'],
-            ['name' => 'Hyundai'],
-            ['name' => 'Toyota'],
-            ['name' => 'Honda'],
-            ['name' => 'Jeep'],
-            ['name' => 'Peugeot'],
-            ['name' => 'Citroën'],
-            ['name' => 'Mitsubishi'],
-            ['name' => 'Nissan'],
-            ['name' => 'Caoa Chery'],
-            ['name' => 'Kia'],
-            ['name' => 'BMW'],
-            ['name' => 'Mercedes-Benz'], // Pode ser separado para carros e pesados se necessário
-            ['name' => 'Audi'],
-            ['name' => 'Volvo'], // Pode ser separado para carros e pesados
-            ['name' => 'Land Rover'],
-            ['name' => 'Ram'],
-            ['name' => 'GWM'],
-            // Marcas de Pesados / Utilitários comuns em Prefeituras
-            ['name' => 'Scania'],
-            ['name' => 'Iveco'],
-            ['name' => 'Agrale'],
-            // Motocicletas (comuns em algumas frotas de prefeitura)
-            ['name' => 'Yamaha'],
-            // Adicione mais marcas conforme necessário
+            ['name' => 'Citroen'],
         ];
 
         foreach ($brands as $brand) {
